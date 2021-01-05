f31dc8022c0558fdc9ff7867074e52251e2b2a77
Jeter_iMac <jeter.lin1208@gmail.com>
2021-01-05 21:28:35 +0800

    Update jira_scraper.py
    Major update

--- jira_scraper.py
+++ jira_scraper.py
@@ -1,34 +1,80 @@
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
+from openpyxl import load_workbook
+from openpyxl import Workbook
 
-driver = webdriver.Chrome()
-url = f'https://matsjira.cienetcorp.com/login.jsp'
 
-driver.get(url)
+class Scraper():
+    def __init__(self, TCid, test_case_list, output_name):
+        self.tc_list_sheet = (load_workbook(str(test_case_list))).active
+        self.output_name = str(output_name)
+        self.wb = Workbook()
 
-# log in to jira
-username = 'jeter.lin'
-pw = 'sD4T1pDTZp'
-driver.find_element_by_id('login-form-username').send_keys(username)
-driver.find_element_by_id('login-form-password').send_keys(pw)
-driver.find_element_by_id('login-form-submit').click()
+    def case_list(self):
+        id_list = []
+        for id in self.tc_list_sheet.iter_rows(max_col=1, values_only=True):
+            if id is not None:
+                id_list.append(id)
+            else:
+                break
+        return id_list
 
-def url_gen(TCid):
-    frame = f'https://matsjira.cienetcorp.com/issues/?jql=project%20%3D%20TESTPLAN%20AND%20%22TEST%20PLAN%20NAME%22%20~%20MY22GB_NonFunc_96I-W157.2.3-QIH22B-230_User_W52%20AND%20text%20~%20'
-    return frame + TCid
+    def scrapping(self):
+        print('Opening Jira...')
+        try:
+            driver = webdriver.Chrome()
+            url = f'https://matsjira.cienetcorp.com/login.jsp'
+            driver.get(url)
+        except:
+            raise SystemExit(
+                'Unable to connect to Jira server, please check your wifi setting!')
 
-driver.get(f'https://matsjira.cienetcorp.com/issues/?jql=project%20%3D%20TESTPLAN%20AND%20%22TEST%20PLAN%20NAME%22%20~%20MY22GB_NonFunc_96I-W157.2.3-QIH22B-230_User_W52%20AND%20text%20~%20TC_MFL_76009_Wireless_Connectivity_0078')
+        username = 'jeter.lin'
+        pw = 'sD4T1pDTZp'
+        print('Entering the username and password')
+        driver.find_element_by_id('login-form-username').send_keys(username)
+        driver.find_element_by_id('login-form-password').send_keys(pw)
+        driver.find_element_by_id('login-form-submit').click()
 
-original_TCID = driver.find_element_by_class_name('customfield_10202').text
-result = driver.find_element_by_class_name('customfield_10341').text
-precondition = driver.find_element_by_class_name('customfield_10331').text
-test_step = driver.find_element_by_class_name('customfield_10342').text
-expected = driver.find_element_by_class_name('customfield_10315').text
-objective = driver.find_element_by_class_name('customfield_10336').text
+        sheet = self.wb.active
+        sheet.create_sheet('Not found')
+        sheet.create_sheet('Detailed list')
 
-print(original_TCID)
-print(precondition)
-print(test_step)
-print(expected)
-print(objective)
+        id_list = self.case_list()
+        cur_num = 0
+        total_tc = len(id_list)
+
+        for id in id_list:
+            cur_num += 1
+            print('fatching...{}/{}'.format(cur_num, total_tc))
+            driver.get(self.url_gen(id))
+            try:
+                original_TCID = driver.find_element_by_class_name(
+                    'customfield_10202').text
+                result = driver.find_element_by_class_name(
+                    'customfield_10341').text
+                precondition = driver.find_element_by_class_name(
+                    'customfield_10331').text
+                test_step = driver.find_element_by_class_name(
+                    'customfield_10342').text
+                expected = driver.find_element_by_class_name(
+                    'customfield_10315').text
+                objective = driver.find_element_by_class_name(
+                    'customfield_10336').text
+                case_detail = [original_TCID, result,
+                               precondition, test_step, expected, objective]
+                print('Found!')
+                print('==========================================')
+                sheet['Detailed list'].append(case_detail)
+            except:
+                print('cannot find the detail of case: {}'.format(id))
+                print('==========================================')
+                sheet['Not found'].append([id])
+
+        print('Done!, saving the file named {}'.format(self.output_name))
+        sheet.save(self.output_name)
+
+    def url_gen(self, tcid):
+        frame = f'https://matsjira.cienetcorp.com/issues/?jql=project%20%3D%20%22TESTSPEC%20MY22%22%20AND%20text%20~%20'
+        return frame + tcid
