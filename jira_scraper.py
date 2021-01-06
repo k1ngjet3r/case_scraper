6ab3b5663582b25f7b52dedc7938f32fc33e33ce
Jeter@work <43425262+k1ngjet3r@users.noreply.github.com>
2021-01-06 14:18:29 +0800

    01/06 stable release

--- jira_scraper.py
+++ jira_scraper.py
@@ -6,7 +6,7 @@ from openpyxl import Workbook
 
 
 class Scraper():
-    def __init__(self, TCid, test_case_list, output_name):
+    def __init__(self, test_case_list, output_name):
         self.tc_list_sheet = (load_workbook(str(test_case_list))).active
         self.output_name = str(output_name)
         self.wb = Workbook()
@@ -15,7 +15,7 @@ class Scraper():
         id_list = []
         for id in self.tc_list_sheet.iter_rows(max_col=1, values_only=True):
             if id is not None:
-                id_list.append(id)
+                id_list.append(str(id)[2:-3])
             else:
                 break
         return id_list
@@ -37,13 +37,13 @@ class Scraper():
         driver.find_element_by_id('login-form-password').send_keys(pw)
         driver.find_element_by_id('login-form-submit').click()
 
-        sheet = self.wb.active
-        sheet.create_sheet('Not found')
-        sheet.create_sheet('Detailed list')
+        self.wb.active
+        self.wb.create_sheet('Not found', 0)
+        self.wb.create_sheet('Detailed list', 0)
 
-        id_list = self.case_list()
+        id_list = self.case_list()[1:]
         cur_num = 0
-        total_tc = len(id_list)
+        total_tc = len(id_list)-1
 
         for id in id_list:
             cur_num += 1
@@ -52,6 +52,7 @@ class Scraper():
             try:
                 original_TCID = driver.find_element_by_class_name(
                     'customfield_10202').text
+                bug_id = driver.find_element_by_class_name('customfield_10212').text
                 result = driver.find_element_by_class_name(
                     'customfield_10341').text
                 precondition = driver.find_element_by_class_name(
@@ -62,19 +63,24 @@ class Scraper():
                     'customfield_10315').text
                 objective = driver.find_element_by_class_name(
                     'customfield_10336').text
-                case_detail = [original_TCID, result,
-                               precondition, test_step, expected, objective]
+                test_plan_name = driver.find_element_by_class_name('customfield_10340').text
+                case_detail = [original_TCID, result, bug_id,
+                               precondition, test_step, expected, objective, test_plan_name]
                 print('Found!')
                 print('==========================================')
-                sheet['Detailed list'].append(case_detail)
+                self.wb['Detailed list'].append(case_detail)
             except:
                 print('cannot find the detail of case: {}'.format(id))
                 print('==========================================')
-                sheet['Not found'].append([id])
+                self.wb['Not found'].append([id])
 
         print('Done!, saving the file named {}'.format(self.output_name))
-        sheet.save(self.output_name)
+        self.wb.save(self.output_name)
 
     def url_gen(self, tcid):
-        frame = f'https://matsjira.cienetcorp.com/issues/?jql=project%20%3D%20%22TESTSPEC%20MY22%22%20AND%20text%20~%20'
-        return frame + tcid
+        frame = f'https://matsjira.cienetcorp.com/issues/?jql=project%20%3D%20TESTPLAN%20AND%20%22EXECUTION%20DATE%22%20%3E%3D%20endOfWeek(-3)%20AND%20%22EXECUTION%20DATE%22%20%3C%3D%20endOfWeek(-2)%20AND%20%22TEST%20PLAN%20NAME%22%20~%20%22MY22*%22%20AND%20text%20~%20'
+        return frame + str(tcid)
+
+scrp = Scraper('MY22TCs transfer list_TaiPei.xlsx', 'W01_trial.xlsx')
+
+scrp.scrapping()
