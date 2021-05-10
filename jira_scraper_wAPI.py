6b84811b256bc8b45e52f6b51a31b3c79b5bcfa8
k1ngjet3r <jeter.lin1208@gmail.com>
2021-05-11 07:45:13 +0800

    Update jira_scraper_wAPI.py

---
+++ jira_scraper_wAPI.py
@@ -0,0 +1,8 @@
+from atlassian import Jira
+from openpyxl import load_workbook, workbook
+
+jira_instance = Jira(url='http://matsjira.cienetcorp.com/',
+                    username='jeter.lin',
+                    password='sD4T1pDTZp'
+                    )
+
