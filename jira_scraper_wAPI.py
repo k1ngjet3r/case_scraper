e12dbed9c8666a1118b3f353f7e9d9cf377d5368
Jeter Lin <jeter.lin1208@gmail.com>
2021-05-11 14:26:48 +0800

    Update jira_scraper_wAPI.py

--- jira_scraper_wAPI.py
+++ jira_scraper_wAPI.py
@@ -1,9 +1,13 @@
 from atlassian import Jira
 from openpyxl import load_workbook, workbook
 
-jira_instance = Jira(url='http://matsjira.cienetcorp.com/',
+jira_instance = Jira(url='https://matsjira.cienetcorp.com/issues/?jql=project%20%3D%20TESTSPEC22%20AND%20%22Original%20GM%20TC%20ID%22%20%20~%20TC_MFL_MyProfile_Security_None_002',
                     username='jeter.lin',
                     password='sD4T1pDTZp'
                     )
 
-jira_instance.issue(key='TC_MFL_MyProfile_Security_None_002', fields=['summary'])
+jira_instance.issue(key='customfield_10202', fields=['Original GM TC ID','summary'])
+
+result = jira_instance.jql('project = TESTSPEC22 AND "Original GM TC ID"  ~ TC_MFL_MyProfile_Security_None_002', fields=['Original GM TC ID','summary'])
+
+print(result)
